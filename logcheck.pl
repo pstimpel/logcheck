4fe0f1cf96d55b38915f2a6b592e4cb2c841525a
Peter Stimpel <pstimpel+github@gmail.com>
2019-07-19 14:34:30 +0200

    Raise Version, 1.0.9

--- logcheck.pl
+++ logcheck.pl
@@ -27,7 +27,7 @@ $frompart="";
 
 if (defined($logcheckpath)) {
 
-	print "There was change in the configuration starting from version 1.0.5!\n";
+	print "There was change in the configuration starting from version 1.0.9!\n";
 	print "\n";
 	print '$file_whitelist=$logcheckpath."logcheck.whitelist"; is now'."\n";
 	print '$file_whitelist="logcheck.whitelist";'."\n";
@@ -49,7 +49,7 @@ $file_logfilelist = $dirname."/".$file_logfilelist;
 sub head() {
 	print "\n";
 	print "-----------------------------\n";
-	print "This is logcheck.pl V1.0.8\n";
+	print "This is logcheck.pl V1.0.9\n";
 	print "https://peters-webcorner.de\n";
 	print "project hosted on github\n";
 	print "https://github.com/pstimpel/logcheck\n\n";
